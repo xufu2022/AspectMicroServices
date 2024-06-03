14c1d396a0a8f49d912bb86058a87952cee7c7cc
test <xufu2017@gmail.com>
2024-06-03 06:54:35 +0100

    exp

---
+++ ch0/AspireSample/AspireSample.AppHost/Program.cs
@@ -0,0 +1,9 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice").WithReplicas(3);
+
+builder.AddProject<Projects.AspireSample_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService);
+
+builder.Build().Run();
